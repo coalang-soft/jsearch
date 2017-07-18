3c59a83c5cd9a0e92cefa27b81662d2f175c63ca
NetLamp <oplpublishergerman@gmail.com>
2017-07-18 21:20:05 +0200

    Commented the code

--- src/io/github/coalangsoft/jsearch/JSearchEngine.java
+++ src/io/github/coalangsoft/jsearch/JSearchEngine.java
@@ -8,22 +8,43 @@ import io.github.coalangsoft.lib.data.Func;
 import io.github.coalangsoft.lib.data.ImutablePair;
 import io.github.coalangsoft.lib.data.Pair;
 
+/**
+ * A search engine to search through a given list of things by string.
+ * @author Matthias
+ *
+ * @param <R> - The type of objects to search and find.
+ */
 public class JSearchEngine<R> {
 
 	{
 		listeners = new ArrayList<Func<String,Object>>();
 	}
 	
+	//the search index. Every entry is a pair of the query and the matching results.
 	private List<Pair<String, List<R>>> index;
+	//a list of listeners. These are used when a new entry (query string) is added to this search engine.
 	private List<Func<String,Object>> listeners;
 	
+	/**
+	 * Creates an "empty" search engine.
+	 */
 	public JSearchEngine(){
 		index = new ArrayList<Pair<String, List<R>>>();
 	}
+	/**
+	 * Creates a pre-filled search engine.
+	 * @param l the search index to start with.
+	 * Every entry is a pair of the query and the matching results.
+	 */
 	public JSearchEngine(List<Pair<String,List<R>>> l){
 		this.index = l;
 	}
 
+	/**
+	 * Iterates over all registered queries and runs a function for each.
+	 * Also adds the function as a listener. So it is called whenever a new query is added.
+	 * @param f the function.
+	 */
 	public void forAllKeys(Func<String,Object> f){
 		for(int i = 0; i < index.size(); i++){
 			f.call(index.get(i).getA());
@@ -31,13 +52,25 @@ public class JSearchEngine<R> {
 		this.listeners.add(f);
 	}
 
+	/**
+	 * Adds a new key value pair to the search index.
+	 * If the query (key) already exists, the value is added to the matching results.
+	 * Otherwise a new entry for this query is created, with one matching result (value).
+	 * If this is the case, the listeners are called. To add a listener, use {@link #forAllKeys(Func)}
+	 * @param key the search query
+	 * @param value the value to find
+	 */
 	public void add(String key, R value){
 		if(key == null){
+			//Nothing to add.. TODO: warn the user
 			return;
 		}
+		
+		//go through every entry of the search index
 		for(int i = 0; i < index.size(); i++){
 			Pair<String, List<R>> p = index.get(i);
 			if(key.equals(p.getA())){
+				//If the query of this part matches "key", extend its matching result list and finish.
 				p.getB().add(value);
 				return;
 			}
@@ -48,6 +81,13 @@ public class JSearchEngine<R> {
 		listeners(key);
 	}
 	
+	/**
+	 * Searches for a set of queries, and returns the result as a list.
+	 * The results get compared, so if there is the same value found with say two of the queries,
+	 * it appears before the values with one matching query.
+	 * @param query the queries to search by.
+	 * @return the search result.
+	 */
 	public ArrayList<JSearchResult<R>> query(String... query){
 		ArrayList<JSearchResult<R>> result = new ArrayList<JSearchResult<R>>();
 		ArrayList<R> values = new ArrayList<R>();
@@ -69,25 +109,44 @@ public class JSearchEngine<R> {
 		return result;
 	}
 
+	/**
+	 * Utility method to register a search result to the finished search result
+	 * list provided by {@link #query(String...)}
+	 * @param raw the raw data to add to the result list.
+	 * @param result the result list to fill.
+	 * @param values a list containing all values that are already inside the result list.
+	 */
 	private void registerResult(List<R> raw, List<JSearchResult<R>> result,
 			List<R> values) {
 		for(int i = 0; i < raw.size(); i++){
 			R val = raw.get(i);
 			if(values.contains(val)){
+				//This value was already found. Upgrade the search result rank.
 				result.get(values.indexOf(val)).upgrade();
 			}else{
+				//This value is new. Add a new search result for it to the results.
 				values.add(val);
 				result.add(new JSearchResult<R>(val));
 			}
 		}
 	}
 
+	/**
+	 * A shorthand for the {@link #add(String, Object)} method that adds multiple values to the same key.
+	 * @param key
+	 * @param values
+	 */
 	public void addAll(String key, R... values) {
 		for(int i = 0; i < values.length; i++){
 			add(key, values[i]);
 		}
 	}
 	
+	/**
+	 * Clones the entries from one search query and adds them to another one.
+	 * @param query the search query to copy from.
+	 * @param alias the search query to copy to.
+	 */
 	public void alias(String query, String alias){
 		for(int i = 0; i < index.size(); i++){
 			Pair<String, List<R>> p = index.get(i);
--- src/io/github/coalangsoft/jsearch/JSearchResult.java
+++ src/io/github/coalangsoft/jsearch/JSearchResult.java
@@ -1,18 +1,37 @@
 package io.github.coalangsoft.jsearch;
 
+/**
+ * Represents a ranked search result provided by a {@link JSearchEngine}.
+ * @author Matthias
+ *
+ * @param <T> the type of values to find.
+ */
 public class JSearchResult<T> implements Comparable<JSearchResult<T>>{
 	
+	//the rank - higher means found more often
 	private int rank = 0;
+	//the value
 	private final T value;
 	
+	/**
+	 * Creates a search result with rank 0.
+	 * @param value the value
+	 */
 	JSearchResult(T value){
 		this.value = value;
 	}
 	
+	/**
+	 * Upgrades the rank of this search result by one.
+	 */
 	void upgrade(){
 		rank++;
 	}
 	
+	/**
+	 * Returns the rank of this search result. Higher means found more often.
+	 * @return the rank.
+	 */
 	public int getRank(){
 		return rank;
 	}
@@ -31,6 +50,10 @@ public class JSearchResult<T> implements Comparable<JSearchResult<T>>{
 		return "JSearchResult [rank=" + rank + ", value=" + value + "]";
 	}
 
+	/**
+	 * Returns the value of this search result.
+	 * @return the value.
+	 */
 	public T getValue() {
 		return value;
 	}
